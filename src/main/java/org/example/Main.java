9e1ee4571191ed137441926b7147400425ac1954
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2022-11-01 20:33:06 +0530

    Basic Playwrite - how to lunch browser and add dependency

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("http://google.com");
+            System.out.println(page.title());
+            browser.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
